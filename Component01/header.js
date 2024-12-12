beecf489516022b8536fa175117586641ad71836
Ken <zengkenwork@gmail.com>
2024-12-12 10:50:30 +0800

    add Component01 folder

---
+++ Component01/header.js
@@ -0,0 +1,12 @@
+const HeaderComponent = {
+  template: `<div id="brand">
+      Site Name
+      <nav>
+        <ul>
+          <li><a href="index.html">Home</a></li>
+          <li><a href="about.html">About</a></li>
+          <li><a href="contact.html">Contact</a></li>
+        </ul>
+      </nav>
+    </div>`,
+};
